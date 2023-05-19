4e21f254586bae0ddeadfed247fe72c40c5efdbc
Shahi77 <shahiinfinity02@gmail.com>
2023-05-19 16:08:08 +0530

    navigating programatically

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -4,9 +4,22 @@ function ClientProjectsPage() {
     const router = useRouter();
     console.log(router.query);
 
+    function loadProjectHandler() {
+        //load data..
+        router.push({
+            pathname: '/clients/[id]/[clientprojectid]',
+            query: { id: 'max', clientprojectid: 'projectA' }
+
+        })
+        // router.push('/clients/max/projecta')    //router push is like using the link component just programaticaly
+        //router.replace('/clients/max/projecta')  =>then we replace current page with dead one i.e. we can't go back after navigation
+
+    }
+
     return (
         <div>
             <h1>The Project of a given Client </h1>
+            <button>Load Project A</button>
         </div>
     )
 }
