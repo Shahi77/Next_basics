eb8ada30abd8d6e3e950ecc6a3c7ea24cbfa611a
Shahi77 <shahiinfinity02@gmail.com>
2023-05-19 14:25:12 +0530

    building nested dynamic routes

---
+++ pages/clients/[id]/index.js
@@ -0,0 +1,13 @@
+import { useRouter } from "next/router";
+function ClientProjectsPage() {
+
+    const router = useRouter();
+    console.log(router.query);
+
+    return (
+        <div>
+            <h1>The Project of a given Client </h1>
+        </div>
+    )
+}
+export default ClientProjectsPage;
